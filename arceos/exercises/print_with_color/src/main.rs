b1ec359cf2047a3c9acb208ce0679eea8be0a62f
vankari <760238843@qq.com>
2025-05-16 20:21:41 +0800

    commit1

--- arceos/exercises/print_with_color/src/main.rs
+++ arceos/exercises/print_with_color/src/main.rs
@@ -2,9 +2,9 @@
 #![cfg_attr(feature = "axstd", no_main)]
 
 #[cfg(feature = "axstd")]
-use axstd::println;
+use axstd::printlnc;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    println!("[WithColor]: Hello, Arceos!");
+    printlnc!("[WithColor]: Hello, Arceos!");
 }
